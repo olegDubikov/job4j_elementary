35eef3d434d7960ead3976de060cb072130f66da
hp <lars75@inbox.ru>
2022-09-08 12:14:55 +0300

    1.2. Входные данные в тесте [#297750 #340918]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -17,7 +17,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C1X1Then() {
+    public void whenA1B1C1X1Then3() {
         int a = 1;
         int b = 1;
         int c = 1;
@@ -28,7 +28,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA0B1C1X1Then() {
+    public void whenA0B1C1X1Then2() {
         int a = 0;
         int b = 1;
         int c = 1;
@@ -39,7 +39,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C0X1Then() {
+    public void whenA1B1C0X1Then2() {
         int a = 1;
         int b = 1;
         int c = 0;
@@ -50,7 +50,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C1X0then() {
+    public void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
         int c = 1;
