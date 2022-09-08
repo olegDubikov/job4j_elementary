fdc2585ff0b9cb0af401ec0ecd2ea25eb603d9dd
hp <lars75@inbox.ru>
2022-09-08 12:56:46 +0300

    corrected

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,7 +2,7 @@ package ru.job4j.calculator;
 
 public class Fit {
     public static double manWeight(short height) {
-        double  rsl = (height - 100) * 1.15;
+        double rsl = (height - 100) * 1.15;
         return rsl;
     }
 
