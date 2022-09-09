4dc5f2318aa1148021d9a14fe3ffef1a4deda34f
hp <lars75@inbox.ru>
2022-09-09 20:13:40 +0300

    0. Операции сравнения [#235631]

---
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+
+        boolean result = first > second;
+        System.out.println("10>9 это " + result);
+        result = first < second;
+        System.out.println("10<9 это " + result);
+        result = first == second;
+        System.out.println("10=9 это " + result);
+    }
+
+}
