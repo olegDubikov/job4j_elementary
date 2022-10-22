1e030ab1bc49f0f791130265efe05343d0707146
hp <lars75@inbox.ru>
2022-10-22 11:31:58 +0300

    0.1. java.lang.NullPointerException [#219364]

---
+++ src/main/java/ru/job4j/ex/FindMaxLength.java
@@ -0,0 +1,17 @@
+package ru.job4j.ex;
+
+public class FindMaxLength {
+    public static void main(String[] args) {
+        String[] shops = {"Ebay", null, "Amazon", null, "Ozon"};
+        int max = 0;
+        for (String el : shops) {
+            if (el == null) {
+                continue;
+            }
+            if (el.length() > max) {
+                max = el.length();
+            }
+        }
+        System.out.println("Max length : " + max);
+    }
+}
