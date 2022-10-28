b5f435258189103ef26b16db513f12b21eca14f1
hp <lars75@inbox.ru>
2022-10-28 15:50:34 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #355497]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -26,6 +26,8 @@ public class UserStore {
             if (validate(user)) {
                 System.out.println("This user has an access");
             }
+        } catch (UserInvalidException en) {
+            en.printStackTrace();
         } catch (UserNotFoundException e) {
             e.printStackTrace();
         }
